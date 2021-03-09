b528a07494720a37a4a07d497cd7b55d4a063334
Yann <yann@lysden.art>
2021-03-09 15:54:55 +0100

    React 02.2 quest submission

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Contact />
+    <div>
+      <Contact name="Keith Little" avatar="https://randomuser.me/api/portraits/men/37.jpg" online />
+      <Contact name="Clinton Sanchez" avatar="https://randomuser.me/api/portraits/men/60.jpg" />
+      <Contact name="Carmen Edwards" avatar="https://randomuser.me/api/portraits/women/19.jpg" online/>
+    </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Contact.css';
 
 
-function Contact() {
-  const userName = "Sue Harper";
-  const userAvatar = "https://randomuser.me/api/portraits/women/61.jpg";
-
+function Contact(props) {
   return (
     <div className="Contact">
-      <img src={userAvatar} alt={userName} className="avatar" />
+      <img src={props.avatar} alt={props.name} className="avatar" />
       <div>
-        <div className="name">{userName}</div>
+        <div className="name">{props.name}</div>
         <div className="status">
-          <span className="status-online" />
-          <span className="status-text">online</span>
+          <span className={props.online ? "status-online" : "status-offline"} />
+          <span className="status-text">{props.online ? "online" : "offline" }</span>
         </div>
       </div>
     </div>
   );
 }
+
+// Specifies the default values for props:
+Contact.defaultProps = {
+  avatar: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/abf3094e-971d-4be5-899b-b50a0e6290cb/da3ejq1-a03dbbf8-5cd1-4a3d-8126-bba42232ac88.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYWJmMzA5NGUtOTcxZC00YmU1LTg5OWItYjUwYTBlNjI5MGNiXC9kYTNlanExLWEwM2RiYmY4LTVjZDEtNGEzZC04MTI2LWJiYTQyMjMyYWM4OC5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.8M1Smkke2cctcrHYBiCKMuxjQD4M6GqWukDPs_MoxPg',
+  name: 'Grodo Do'
+};
+
+Contact.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  online: PropTypes.bool
+};
 
 export default Contact;
