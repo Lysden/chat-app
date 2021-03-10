d2a010764d31ca997f199e4735e797d52405a1ca
Yann <yann@lysden.art>
2021-03-10 10:15:49 +0100

    React 03 Quest submission

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 
 function App() {
   return (
     <div>
-      <Contact name="Keith Little" avatar="https://randomuser.me/api/portraits/men/37.jpg" online />
-      <Contact name="Clinton Sanchez" avatar="https://randomuser.me/api/portraits/men/60.jpg" />
-      <Contact name="Carmen Edwards" avatar="https://randomuser.me/api/portraits/women/19.jpg" online/>
+      <ContactList />
     </div>
   );
 }
